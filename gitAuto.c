c2888f10b72855383c68cee2148d9fc1615d5f98
Gabriel <gabrielmnakashima2@gmail.com>
2026-05-26 03:54:26 -0300

    Detecta a branch automaticamente e comentarios

--- gitAuto.c
+++ gitAuto.c
@@ -2,20 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+// cores pro cmd bonito
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define CYAN "\033[36m"
+#define RESET "\033[0m"
+
 void helpMenu() {
     printf("Uso:\n");
-    printf("-b nomeDaBranch(main por padrao)\n");
     printf("-m MessagemCommit (Obrigatoria!)\n");
     printf("--help Menu de ajuda\n");
     printf("Exemplos: \n");
     printf(".\\gitAuto.exe \"consertando bug\"\n");
-    printf(".\\gitAuto.exe -b dev -m \"consertando bug\"\n");
+    printf(".\\gitAuto.exe -m \"consertando bug\"\n");
 }
 
-
 int main(int argc, char* argv[]) {
-
-    char branch[100] = "master";
+    char branch[100] = "main";
     char message[256] = "";
 
     if(argc == 2 && strcmp(argv[1], "--help") == 0) {
@@ -23,26 +27,37 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    if(argc == 2) {
-        strncpy(message, argv[1], sizeof(message) - 1);
-        message[sizeof(message) - 1] = '\0';
-    } else {
-        for(int i = 1; i < argc; i++) {
-            if(strcmp(argv[i], "-b") == 0 && (i + 1) < argc) {
-                strncpy(branch, argv[++i], sizeof(branch) - 1);
-                branch[sizeof(branch) - 1] = '\0';
-            } else if(strcmp(argv[i], "-m") == 0 && (i + 1) < argc) {
-                strncpy(message, argv[++i], sizeof(message) - 1);
-                message[sizeof(message) - 1] = '\0';
-            } else {
-                printf("Argumento invalido: %s\n", argv[i]);
-                return 1;   
-            }
+    // pega a branch padrao do projeto
+    FILE* f = popen("git branch --show-current", "r");
+    if(f != NULL) {
+        if(fgets(branch, sizeof(branch), f) != NULL) {
+            branch[strcspn(branch, "\n")] = 0;
+        }
+
+        pclose(f);
+    }
+
+    // pega a messagem do commit
+    for(int i = 1; i < argc; i++) {
+        // caso tenha passado a flag -m
+        if(strcmp(argv[i], "-m") == 0 && (i + 1) < argc) {
+            strncpy(message, argv[++i], sizeof(message) - 1);
+            message[sizeof(message) - 1] = '\0';
+        }
+        // caso nao tenha passado a flag
+        else if(message[0] == '\0') {
+            strncpy(message, argv[i], sizeof(message) - 1);
+            message[sizeof(message) - 1] = '\0';
+        } else {
+            // digitou qualquer coisa
+            printf(RED "[ERRO] Argumento invalido: %s" RESET "\n", argv[i]);
+            return 1;
         }
     }
 
-    if(strlen(message) == 0) {
-        printf("Voce precisa informar uma mensagem com -m\n");
+    //mensagem vazia (.\gitAuto)
+    if(message[0] == '\0') {
+        printf(RED "[ERRO] Voce precisa informar uma mensagem! "RESET" \n");
         return 1;
     }
 
@@ -52,33 +67,32 @@ int main(int argc, char* argv[]) {
     char pushCommand[512];
     snprintf(pushCommand, sizeof(pushCommand),"git push origin %s", branch);
 
-    
-    printf("\033[33m[1/3] Adicionando arquivos...\033[0m\n");
+    printf(YELLOW "[1/3] Adicionando arquivos..." RESET "\n");
 
     if(system("git add .") != 0) {
-        printf("\033[31m[ERRO] Falha ao adicionar arquivos!\033[0m\n");
+        printf(RED "[ERRO] Falha ao adicionar arquivos!" RESET "\n");
         return 1;
     }
 
-    printf("\033[32m[OK] Arquivos adicionados!\033[0m\n");
+    printf(GREEN "[OK] Arquivos adicionados!" RESET "\n");
 
-    printf("\033[33m[2/3] Commitando...\033[0m\n");
+    printf(YELLOW "[2/3] Commitando..." RESET "\n");
 
     if(system(commitCommand) != 0) {
-        printf("\033[31m[ERRO] Falha ao criar commit!\033[0m\n");
+        printf(RED "[ERRO] Falha ao criar commit!" RESET "\n");
         return 1;
     }
 
-    printf("\033[36m[INFO] Commit: %s\033[0m\n", message);
-    printf("\033[32m[OK] Commit realizado!\033[0m\n");
+    printf(CYAN "[INFO] Commit: %s" RESET "\n", message);
+    printf(GREEN "[OK] Commit realizado!" RESET "\n");
 
-    printf("\033[33m[3/3] Enviando para %s...\033[0m\n", branch);
+    printf(YELLOW "[3/3] Enviando para %s..." RESET "\n", branch);
 
     if(system(pushCommand) != 0) {
-        printf("\033[31m[ERRO] Falha ao enviar para o repositorio!\033[0m\n");
+        printf(RED "[ERRO] Falha ao enviar para o repositorio!" RESET "\n");
         return 1;
     }
 
-    printf("\033[32m[OK] Codigo enviado com sucesso!\033[0m\n");
+    printf(GREEN "[OK] Codigo enviado com sucesso!" RESET "\n");
     return 0;
 }
